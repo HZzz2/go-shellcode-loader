556f6ddeb1fc736aebe81448339014cb7effc97b
leslie <hzzz2yb@gmail.com>
2022-07-28 17:40:04 +0800

    Update go-sc.go

--- go-sc.go
+++ go-sc.go
@@ -24,16 +24,7 @@ func build(ddm string) {
 
 }
 
-//填充字符串（末尾）
-func PaddingText1(str []byte, blockSize int) []byte {
-	//需要填充的数据长度
-	paddingCount := blockSize - len(str)%blockSize
-	//填充数据为：paddingCount ,填充的值为：paddingCount
-	paddingStr := bytes.Repeat([]byte{byte(paddingCount)}, paddingCount)
-	newPaddingStr := append(str, paddingStr...)
-	//fmt.Println(newPaddingStr)
-	return newPaddingStr
-}
+
 
 //去掉字符（末尾）
 func UnPaddingText1(str []byte) []byte {
